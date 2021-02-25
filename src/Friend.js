8d7769155790d4610616c77dcf1a9a01008d77a5
Atharva Deshpande <atharvadeshpande174@gmail.com>
2021-02-25 22:42:11 +0530

    Slack Version 2.0 with new one to one chat facility

---
+++ src/Friend.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react'
+import './Channel.css'
+import InfoIcon from '@material-ui/icons/Info';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import Messages from './Messages';
+import { useStateValue } from './StateProvider';
+import { useParams } from 'react-router-dom';
+import db, { timeStamp } from './firebase';
+function Friend() {
+    const {friendId} = useParams();
+    const [channelName,setChannelName] = useState();
+    const [sendPlaceholder,setSendPlaceholder] = useState('');
+    const [{user},dispatch] = useStateValue();
+    const [input,setInput] = useState('');
+    const [messages,setMessages] = useState([]);
+    const getMessages = async ()=> {
+            await db.collection("users").doc(user.email).collection("friends").doc(friendId).onSnapshot(snapshot=>(setChannelName(snapshot.data().name)));
+            await db.collection("users").doc(user.email).collection("friends").doc(friendId).collection("messages").orderBy('timestamp',"asc").onSnapshot((snapshot)=>{
+               setMessages(snapshot.docs.map(doc=>doc.data()))
+           })
+    }
+    const send = (e) => {
+        e.preventDefault();
+        if (input!=='')
+        {
+            if (friendId != user.email)
+            {
+                db.collection("users").doc(user.email).collection("friends").doc(friendId).collection("messages").add(
+                {
+                    message: input,
+                    name: user.displayName,
+                    email: user.email,
+                    photoURL: user.photoURL,
+                    timestamp: timeStamp,
+                }
+            )
+            }
+            
+            db.collection("users").doc(friendId).collection("friends").doc(user.email).collection("messages").add(
+                {
+                    message: input,
+                    name: user.displayName,
+                    email: user.email,
+                    photoURL: user.photoURL,
+                    timestamp: timeStamp,
+                }
+            )
+            setInput('');
+        }
+    }
+    useEffect(()=>{
+        if (friendId)
+        {
+            getMessages();
+            setSendPlaceholder("Send a message to #" + channelName);
+        }
+    },[friendId,channelName])
+    return (
+        <div className = "channel">
+            <div className = "channel__header">
+                {channelName?(<h1>{channelName}</h1>):( <h1>Channel Name</h1>)}
+                <div>
+                    <PersonAddIcon/>
+                    <InfoIcon/>
+
+                </div>
+            </div>
+            <div className = "channel__messages">
+                
+                {messages.map(message=>(
+                    <Messages sendersName = {message.name} message = {message.message} photoURL = {message.photoURL} email = {message.email} timestamp = {message.timestamp}/>
+                ))}
+                {/* <Messages sendersName = "Anonymous" message = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aut perspiciatis, laudantium officia aliquam voluptatum cupiditate dolore quasi consectetur quo magnam distinctio numquam dicta iste rerum nihil veritatis praesentium obcaecati."/>
+                <Messages sendersName = "Atharva Deshpande" message = "Whats Up"/>
+                */}
+                
+
+
+            </div>
+
+            <div className = "channel__send">
+                <form>
+                    <input value={input} onChange={e => setInput(e.target.value)}  className = "sendMessage" type = "text" placeholder = {sendPlaceholder} />
+                    <button onClick  = {send}>Send</button>
+                </form>
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default Friend;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,34 +1,73 @@
 import React, { useEffect, useState } from 'react'
 import './Sidebar.css'
 import AddBoxIcon from '@material-ui/icons/AddBox';
+import AddIcon from '@material-ui/icons/Add';
 import { Tooltip } from '@material-ui/core';
 import ChannelName from './ChannelName';
 import db from './firebase';
+import FriendName from './FriendName';
+import { useStateValue } from './StateProvider';
 function Sidebar() {
     const[channels,setChannels]= useState([]);
+    const [friends,setFriends] = useState([]);
+    const [{user},dispatch] = useStateValue();
     const addNewChannel = () => {
         const channelName = prompt("Enter Channel Name")
         if (channelName) {
             db.collection("channels").add({name: channelName});
         }
     }
+    const addNewFriend = () => {
+        const friendId = prompt("Enter EmailId")
+        const friendName = prompt("Enter Name")
+        if (friendId) {
+            // db.collection("users").doc(friendId)?.onSnapshot(snapshot=>(setNewFriendName(snapshot.data().name)));
+            db.collection("users").doc(friendId).collection("friends").doc(user.email).set(
+                {
+                    name: user.displayName,
+                    email: user.email
+                }
+            );
+            db.collection("users").doc(user.email).collection("friends").doc(friendId).set(
+                {
+                    name: friendName,
+                    email: friendId
+                }
+            )
+        }
+    }
     useEffect(()=>{
-       db.collection("channels").onSnapshot((snapshot)=>{
+      
+        db.collection("channels").onSnapshot((snapshot)=>{
            setChannels(snapshot.docs.map(doc=>(
                {
                    id: doc.id,
                    data: doc.data(),
                }
            )))
-       }) 
+        });
+        db.collection("users").doc(user.email).collection("friends").onSnapshot((snapshot)=>{
+        setFriends(snapshot.docs.map(doc=>(
+            {
+                id: doc.id,
+                data: doc.data(),
+            }
+        )))
+    });
     },[]);
     return (
         <div className = "sidebar">
             <div className = "sidebar__header">
                 <p className = "sidebar__header__title">Workspace</p>
+                <div>
+                    <Tooltip title = "New Conversation">
+                    <AddIcon onClick = {addNewFriend}/>
+                </Tooltip>
                 <Tooltip title = "New Channel">
                     <AddBoxIcon onClick = {addNewChannel}/>
                 </Tooltip>
+                </div>
+                
             </div>
             <div className = "sidebar__channels">
                 <b>Channels : </b>
@@ -38,6 +77,12 @@ function Sidebar() {
                         <ChannelName id = {channel.id} name = {channel.data.name}/>
                  
                    
+                ))}
+            </div>
+            <div className = "sidebar__channels">
+                <b>Direct Messages : </b>
+                {friends.map(channel=>(
+                        <FriendName id = {channel.id} name = {channel.data.name}/>
                 ))}
             </div>
         </div>
